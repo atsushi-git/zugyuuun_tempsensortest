4a4053b2d8e5f8a5c0a506a56c276fe1d9452156
atsushi-git <cos24boc@gmail.com>
2015-04-21 16:57:24 +0900

    app.js modified for temp sensor

--- app.js
+++ app.js
@@ -3,22 +3,38 @@ ZGN(function()
   // TerminalのSPIインスタンスを取得
   var spi = ZGN.term('1').spi;
 
-  // 送信バッファに[ 0x06, 0x00, 0x00 ]を指定
-  // ※MCP3208のCH0をシングルエンドで動かす場合
-  var txbuf = [ 0x06, 0x00, 0x00 ];
-  var rxbuf = [ 0x00, 0x00, 0x00 ];
+  // ADT7310の場合
+  var txinit = 0x56;
+  var rxinit = 0x00;
+  var txbuf = [ 0x00, 0x00 ];
+  var rxbuf = [ 0x00, 0x00 ];
 
+  //センサ初期化, 計測開始
+  spi.transfer(txinit, rxinit, function(tmp) {
+  });
+
+  //300msのdelay
+  function sleep(time, callback){
+	  setTimeout(callback, time);
+  }
+  sleep(300, function(){});
+
+  //メインのループ関数
   setInterval(function() {
-    // 100ms毎にSPI通信でCH0の状態を取得
+    // 500ms毎にSPI通信でCH0の状態を取得
     spi.transfer(txbuf, rxbuf, function(buf) {
-      var s = 0;
+    	var temp = 0x0000;
+		var itemp = 0;
 
-      // 結果配列（16進数の配列）から整数値を取得
-      for (var i=0; i < buf.length; i++)
-          s += buf[i] << (2-i)*8;
+		// 結果配列（16進数の配列）から整数値を取得
+		temp = buf[0];
+		temp |= buf[1];
+		temp = temp >> 3;
+		itemp = (int)temp;
+		itemp /= 16;
 
-      // 結果を表示
-      $('#status').text(s);
-    });
-  }, 100);
+    	// 結果を表示
+    	$('#status').text(s);
+  	});
+  }, 500);
 });
