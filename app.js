d8efc8b294f818671dc0ab33de2252087dd66bac
atsushi-git <cos24boc@gmail.com>
2015-04-21 19:25:33 +0900

    debug

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ ZGN(function()
   var txbuf = [ 0x00, 0x00 ];
   var rxbuf = [ 0x00, 0x00 ];
 
+  var tx3 = [0x54, 0x00, 0x00];
+  var tx4 = [0x00, 0x00, 0x00];
+
   //delay
   function sleep(time, callback){
 	  setTimeout(callback, time);
   }
-  
+ 
+ /* 
   //センサの精度を16bitに変更
   spi.transfer([0x08, 0x80], [0x00, 0x00], function(tmp) {
   });
@@ -25,22 +29,22 @@ ZGN(function()
   });
 
   sleep(500, function(){});
+ */
 
   //メインのループ関数
   setInterval(function() {
     // 500ms毎にSPI通信でCH0の状態を取得
-    spi.transfer(txbuf, rxbuf, function(buf) {
-    	var temp = 0x0000;
-		var itemp = 0;
+    spi.transfer(tx3, rx3, function(buf) {
+    	var temp = 0;
 
 		// 結果配列（16進数の配列）から整数値を取得
-		temp = buf[0] << 8;
-		temp |= buf[1];
+		temp = buf[1] << 8;
+		temp += buf[2];
 		//temp = temp >> 3;
-		itemp = temp / 16;
+		//itemp = temp / 16;
 
     	// 結果を表示
-    	$('#status').text(itemp);
+    	$('#status').text(temp);
   	});
   }, 500);
 });
